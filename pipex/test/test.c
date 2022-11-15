015badce3ca89995ef0a49d60c043e0957da0653
SaveMePlz <chuashixiang@gmail.com>
2022-11-15 21:13:57 +0800

    Pipex Testest

--- pipex/test/test.c
+++ pipex/test/test.c
@@ -20,12 +20,21 @@ int	parent_process(int counter, int pipe[2], status *rwstatus)
 	printf("from child to parent = ");
 	printf("%s", BUFF);
 	printf("\n");
+	waitpid();
+	printf("Parent wrote stuff\n");
+	write(pipe[1], "SUS", 3);
 }
 
 int	child_process(int counter, int pipe[2], status *rwstatus)
 {
+	char BUFF[100];
 	printf("child wrote stuff\n");
 	write(pipe[1], "AMOGUS", 7);
+	int check = read(pipe[0], BUFF, 100);
+	BUFF[check] = '\0';
+	printf("From parent");
+	printf("%s\n", BUFF);
+	read(pipe[0], BUFF, 100);
 }
 
 int main(int ac, char **av)
