cc630d6c50d27758dc05d38180d41b6fa9d4aadc
Chua Shi Xiang <cshi-xia@u82z01s02.42kl.edu.my>
2022-12-01 18:09:08 +0800

    partially working philo

--- philo/prototype 2/philo.c
+++ philo/prototype 2/philo.c
@@ -50,24 +50,26 @@ void	philo_init(t_philo	*philo_data, t_data	*data)
 		philo_data->next_id = philo_data->id + 1;
 }
 
-void	print_timestamp(t_philo *philo_data, char *msg)
-{
-	printf(msg, (gettime() - philo_data->start_time) / 1000, philo_data->id);
-}
-
 void	philoeat(t_data *data, t_philo *philo_data)
 {
-	print_timestamp(philo_data, "%ld %d is eating\n");
-	philo_data->last_eaten = gettime();
-	if (data->eat_count[philo_data->id - 1] > 0)
-		data->eat_count[philo_data->id - 1]--;
-	usleep(data->time_to_eat);
+	if (data->start_sim)
+	{
+		print_timestamp(data, philo_data, "is eating\n");
+		philo_data->last_eaten = gettime();
+		// philo_data->start_time = philo_data->last_eaten;
+		if (data->eat_count[philo_data->id - 1] > 0)
+			data->eat_count[philo_data->id - 1]--;
+		usleep(data->time_to_eat);
+	}
 }
 
 void	philosleep(t_data *data, t_philo *philo_data)
 {
-	print_timestamp(philo_data, "%ld %d is sleeping\n");
-	usleep(data->time_to_sleep);
+	if (data->start_sim)
+	{
+		print_timestamp(data, philo_data, "is sleeping\n");
+		usleep(data->time_to_sleep);
+	}
 }
 
 int	philodeath(void *arg)
@@ -79,12 +81,14 @@ int	philodeath(void *arg)
 	package = (t_arg *) arg;
 	data = package->data;
 	philo_data = package->philo_data;
-	while (!data->someone_died)
+	while (data->start_sim)
 	{
-		if ((gettime() - philo_data->last_eaten) > data->time_to_die)
+		usleep(data->time_to_die);
+		if (data->start_sim && !philo_data->can_eat && (gettime() - philo_data->last_eaten) > data->time_to_die)
 		{
-			data->someone_died = TRUE;
-			print_timestamp(philo_data, "%ld %d died\n");
+			data->start_sim = FALSE;
+			print_timestamp(data, philo_data, "died\n");
+			break;
 		}
 	}
 	return (0);
@@ -92,33 +96,35 @@ int	philodeath(void *arg)
 
 void	philothink(t_data *data, t_philo *philo_data)
 {
-	print_timestamp(philo_data, "%ld %d is thinking\n");
+	if (data->start_sim)
+		print_timestamp(data, philo_data, "is thinking\n");
 }
 
 void	get_fork(t_data *data, t_philo *philo_data)
 {
 	// get fork
-	printf("%d, %d\n", philo_data->id, philo_data->next_id);
-	if (philo_data->id != philo_data->next_id)
+	if (!philo_data->can_eat && philo_data->id != philo_data->next_id)
 	{
 		pthread_mutex_lock(&data->forks[philo_data->id - 1]);
-		print_timestamp(philo_data, "%ld %d has taken a fork\n");
+		print_timestamp(data, philo_data, "has taken a fork\n");
 		pthread_mutex_lock(&data->forks[philo_data->next_id - 1]);
-		print_timestamp(philo_data, "%ld %d has taken a fork\n");
+		print_timestamp(data, philo_data, "has taken a fork\n");
 		philo_data->can_eat = TRUE;
+		return ;
 	}
 
 	if (philo_data->can_eat)
 	{
+		philo_data->can_eat = FALSE;
 		philoeat(data, philo_data);
 
 		// return fork
 		pthread_mutex_unlock(&data->forks[philo_data->id - 1]);
 		pthread_mutex_unlock(&data->forks[philo_data->next_id - 1]);
 
-		philo_data->can_eat = FALSE;
 		philosleep(data, philo_data);
 		philothink(data, philo_data);
+		return ; 
 	}
 }
 
@@ -136,7 +142,22 @@ int	check_count(t_data *data)
 	return (1);
 }
 
-int	run_sim(void *arg)
+void	check_full(void *arg)
+{
+	t_data *data;
+
+	data = (t_data *) arg;
+	while (1)
+	{
+		if (check_count(data))
+		{
+			data->start_sim = FALSE;
+			break;
+		}
+	}
+}
+
+void	run_sim(void *arg)
 {
 	t_data	*data;
 	t_philo *philo_data;
@@ -144,19 +165,14 @@ int	run_sim(void *arg)
 
 	package = (t_arg *) arg;
 	data = package->data;
-	philo_data = package->philo_data; 
+	philo_data = package->philo_data;
 	if (philo_data->id % 2)
 	{
 		philothink(data, philo_data);
 		usleep(data->time_to_sleep / 2);
 	}
-	while (!data->someone_died)
-	{
-		if (data->eat_count[0] != -1)
-			if (check_count(data))
-				break;
+	while (data->start_sim)
 		get_fork(data, philo_data);
-	}
 }
 
 int	philo(void *arg)
@@ -182,12 +198,8 @@ int	philo(void *arg)
 	philo_data.last_eaten = philo_data.start_time;
 	pthread_create(&philo_data.main_thread, NULL, (void *)run_sim, &package);
 	pthread_create(&philo_data.death_thread, NULL, (void *)philodeath, &package);
-	// while (1)
-	// 	if (data->someone_died)
-	// 	{
-	// 		pthread_detach(data->philo_id[philo_data.id - 1]);
-	// 		break;
-	// 	}]
+	pthread_create(&philo_data.check_thread, NULL, (void *)check_full, data);
+	pthread_join(philo_data.check_thread, NULL);
 	pthread_join(philo_data.main_thread, NULL);
 	pthread_join(philo_data.death_thread, NULL);
 	return (1);
@@ -230,12 +242,14 @@ void	init_data(t_data *data, int	n_philo, int time_to_die, int time_to_eat, int 
 	data->time_to_die = time_to_die;
 	data->time_to_sleep = time_to_sleep;
 	data->time_to_eat = time_to_eat;
+
 	data->eat_count = init_eatcount(n_philo, eat_count);
 	data->forks = init_forks(n_philo);
-	data->someone_died = FALSE;
 	data->start_sim = FALSE;
 	data->philo_id = malloc ((sizeof(pthread_t) * n_philo));
+
 	pthread_mutex_init(&data->pick_fork, NULL);
+	pthread_mutex_init(&data->write_data, NULL);
 }
 
 int	main(int ac, char **av)
--- philo/prototype 2/philo.h
+++ philo/prototype 2/philo.h
@@ -11,7 +11,7 @@
 
 #define TRUE 1
 #define FALSE 0
-typedef	unsigned char bool;
+typedef	unsigned char status;
 
 typedef	struct s_philo
 {
@@ -19,10 +19,11 @@ typedef	struct s_philo
 	int			next_id;
 	long int	start_time;
 	long int	last_eaten;
-	bool		can_eat;
-	bool		is_thinking;
+	status		can_eat;
+	status		is_thinking;
 	pthread_t	death_thread;
 	pthread_t	main_thread;
+	pthread_t	check_thread;
 } t_philo;
 
 typedef struct s_data
@@ -33,19 +34,21 @@ typedef struct s_data
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				*eat_count;
-	long int		start_time;
 	pthread_t		*philo_id;
 
-	bool			start_sim;
-	bool			someone_died;
+	status			start_sim;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	pick_fork;
+	pthread_mutex_t	write_data;
 } t_data;
 
 typedef	struct s_arg
 {
 	t_philo	*philo_data;
-	t_data	*data;	
+	t_data	*data;
 } t_arg;
+
+long int	gettime();
+void	print_timestamp(t_data *data, t_philo *philo_data, char *msg);
 
 #endif
