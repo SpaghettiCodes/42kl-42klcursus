7a91f353c42d08929ed0bcd171507b05f44d96a9
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-01-17 16:07:22 +0800

    made init.c

--- 3third/fdf/imac_proto/fdf.c
+++ 3third/fdf/imac_proto/fdf.c
@@ -1,17 +1,5 @@
 #include "fdf.h"
 
-t_coordinates *init()
-{
-	t_coordinates *ret;
-
-	ret = (t_coordinates *) malloc ( sizeof( t_coordinates ) );
-	ret->next = NULL;
-	ret->debug_link = NULL;
-	ret->debug_link2 = NULL; 
-	ret->below = NULL;
-	return (ret);
-}
-
 t_coordinates *fill_node(t_coordinates *current, int x, int y, int z)
 {
 	current->coord[X] = x;
@@ -192,7 +180,6 @@ void	freebuffer(int **tab, t_mlx mlx)
 	free(tab);
 }
 
-
 int	render_next_frame(t_mlx *mlx)
 {
 	hook_values(mlx);
@@ -206,39 +193,6 @@ int	render_next_frame(t_mlx *mlx)
 	if (mlx->window)
 		mlx_put_image_to_window(mlx->info, mlx->window, mlx->image.img, 0, 0);
 	mlx_destroy_image(mlx->info, mlx->image.img);
-}
-
-void	init_attri(t_mlx mlx, t_attri *attr, t_coordinates *coordinate)
-{
-	attr->light_dis = LIGHT_DIS;
-	attr->rot[X] = 0;
-	attr->rot[Y] = 0;
-	attr->rot[Z] = 0;
-	attr->line_size = LINE_SIZE;
-	attr->x_translation = mlx.length / 2;
-	attr->y_translation = mlx.width / 2;
-	attr->x_mid = highest_x(coordinate) / 2;
-	attr->y_mid = highest_y(coordinate) / 2;
-	attr->z_mid = 0;
-	attr->z_multiplier = Z_MULTI;
-	attr->color_intens = MULITIPLIER;
-	attr->type = 'o';
-	attr->color_type = RG;
-}
-
-void	init_keys(t_keypress *key_press)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 4)
-		key_press->kbrd[i] = RELEASED;
-	key_press->mse[L_MSE] = RELEASED;
-	key_press->mse[R_MSE] = RELEASED;
-	key_press->lmse_diff[X] = 0;
-	key_press->lmse_diff[Y] = 0;
-	key_press->rmse_diff[X] = 0;
-	key_press->rmse_diff[Y] = 0;
 }
 
 void	free_coordinate(t_coordinates **stuff)
---
+++ 3third/fdf/imac_proto/init.c
@@ -0,0 +1,46 @@
+#include "fdf.h"
+
+void	init_attri(t_mlx mlx, t_attri *attr, t_coordinates *coordinate)
+{
+	attr->light_dis = LIGHT_DIS;
+	attr->rot[X] = 0;
+	attr->rot[Y] = 0;
+	attr->rot[Z] = 0;
+	attr->line_size = LINE_SIZE;
+	attr->x_translation = mlx.length / 2;
+	attr->y_translation = mlx.width / 2;
+	attr->x_mid = highest_x(coordinate) / 2;
+	attr->y_mid = highest_y(coordinate) / 2;
+	attr->z_mid = 0;
+	attr->z_multiplier = Z_MULTI;
+	attr->color_intens = MULITIPLIER;
+	attr->type = 'o';
+	attr->color_type = RG;
+}
+
+void	init_keys(t_keypress *key_press)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 4)
+		key_press->kbrd[i] = RELEASED;
+	key_press->mse[L_MSE] = RELEASED;
+	key_press->mse[R_MSE] = RELEASED;
+	key_press->lmse_diff[X] = 0;
+	key_press->lmse_diff[Y] = 0;
+	key_press->rmse_diff[X] = 0;
+	key_press->rmse_diff[Y] = 0;
+}
+
+t_coordinates *init()
+{
+	t_coordinates *ret;
+
+	ret = (t_coordinates *) malloc ( sizeof( t_coordinates ) );
+	ret->next = NULL;
+	ret->debug_link = NULL;
+	ret->debug_link2 = NULL; 
+	ret->below = NULL;
+	return (ret);
+}
