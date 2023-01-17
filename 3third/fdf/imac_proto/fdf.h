486badfc8a4e99d82ec3f9cf3460bb1bea0f3139
Please Remember To Change In the Future <sexamogus@notarealemail.com>
2023-01-17 16:42:23 +0800

    removed redundant

--- 3third/fdf/imac_proto/fdf.h
+++ 3third/fdf/imac_proto/fdf.h
@@ -144,28 +144,25 @@ typedef struct s_lineutils
 #define LINE_SIZE 25
 #define Z_MULTI 1
 
-void		project(t_mlx *mlx, char type);
-int			highest_x(t_coordinates *coordinates);
-int			highest_y(t_coordinates *coordinates);
-int			highest_z(t_coordinates *coordinates);
-int			lowest_z(t_coordinates *coordinates);
-int			hook_me_up(t_mlx *mlx);
-int			set_color(int plot_y, t_coordinates *o_point, t_coordinates *n_point, t_attri attr);
+void			project(t_mlx *mlx, char type);
+int				highest_x(t_coordinates *coordinates);
+int				highest_y(t_coordinates *coordinates);
+int				highest_z(t_coordinates *coordinates);
+int				lowest_z(t_coordinates *coordinates);
+int				hook_me_up(t_mlx *mlx);
+int				set_color(int plot_y, t_coordinates *o_point, t_coordinates *n_point, t_attri attr);
 
-int			not_valid(int x, int y, t_data img);
-void		placepixel(t_data *data, int x, int y, int z, int color);
-int			putcolor(unsigned char trans, unsigned char r, unsigned char g, unsigned char b);
-int			skipx(int startx, t_data img_prop, t_lineutils *line);
-int			skipy(int startx, t_data img_prop, t_lineutils *line);
-void		next_ll(t_lineutils *line);
-void		next_hl(t_lineutils *line);
-void		draw_low_line(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
-void		draw_high_line(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
-void		draw_vertical(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
-void		draw_line_handler(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
-void		fill_image(t_mlx *mlx, t_coordinates *points);
-void		freebuffer(int **tab, t_mlx mlx);
-void		free_coordinate(t_coordinates **stuff);
+void			placepixel(t_data *data, int x, int y, int z, int color);
+int				putcolor(unsigned char trans, unsigned char r, unsigned char g, unsigned char b);
+
+// bersenham line algo
+void			next_ll(t_lineutils *line);
+void			next_hl(t_lineutils *line);
+void			draw_low_line(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
+void			draw_high_line(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
+void			draw_vertical(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
+void			draw_line_handler(t_coordinates *o_point, t_coordinates *n_point, t_mlx *mlx);
+void			fill_image(t_mlx *mlx, t_coordinates *points);
 
 // mouse
 int			mouse_press(int	keycode, int x, int y, t_mlx *mlx);
@@ -173,21 +170,18 @@ int			mouse_released(int keycode, int x, int y, t_keypress *key_press);
 int			mouse_movement(int x, int y, t_keypress *key_press);
 
 // colors
-void		color_choice(int *r, int *g, int *b, t_attri attr);
-int			det_color_p(int z, t_attri attr);
-int			det_color_n(int z, t_attri attr);
+void			color_choice(int *r, int *g, int *b, t_attri attr);
+int				det_color_p(int z, t_attri attr);
+int				det_color_n(int z, t_attri attr);
 
 // rotation
-#include "fdf.h"
-
-// rotattat is in radian btw
-int			rotate_x(t_coordinates *current, float radian);
-int			rotate_y(t_coordinates *current, float radian);
-int			rotate_z(t_coordinates *current, float radian);
+int				rotate_x(t_coordinates *current, float radian);
+int				rotate_y(t_coordinates *current, float radian);
+int				rotate_z(t_coordinates *current, float radian);
 
 // out of bounds checker
-int			out_of_bounds(int x, int y, t_data img);
-int			out_of_bounds2(t_coordinates *coord, t_data img);
+int				out_of_bounds(int x, int y, t_data img);
+int				out_of_bounds2(t_coordinates *coord, t_data img);
 
 // init
 void			init_attri(t_mlx mlx, t_attri *attr, t_coordinates *coordinate);
@@ -195,25 +189,27 @@ void			init_keys(t_keypress *key_press);
 t_coordinates	*init();
 
 // get file contents
-t_coordinates *get_points(int	read_file_fd);
+t_coordinates	*get_points(int	read_file_fd);
 
 // free
-void	freeall(char *line, char **coordinates);
+void			freeall(char *line, char **coordinates);
+void			freebuffer(int **tab, t_mlx mlx);
+void			free_coordinate(t_coordinates **stuff);
 
 // key_pressed
-int	handle_pressed2(int keycode, t_mlx *mlx);
-int	handle_pressed(int keycode, t_mlx *mlx);
-int	handle_released(int keycode, t_keypress *key_press);
+int				handle_pressed2(int keycode, t_mlx *mlx);
+int				handle_pressed(int keycode, t_mlx *mlx);
+int				handle_released(int keycode, t_keypress *key_press);
 
 // z_buffer functions
-int		**z_buffer(t_mlx mlx);
-void	freebuffer(int **tab, t_mlx mlx);
-void	set(int **tab, t_mlx *mlx);
+int				**z_buffer(t_mlx mlx);
+void			freebuffer(int **tab, t_mlx mlx);
+void			set(int **tab, t_mlx *mlx);
 
-void	reset_rot(t_mlx *mlx);
+void			reset_rot(t_mlx *mlx);
 
-void	color_type(t_mlx *mlx);
+void			color_type(t_mlx *mlx);
 
-int	hook_values(t_mlx *mlx);
+int				hook_values(t_mlx *mlx);
 
 #endif
