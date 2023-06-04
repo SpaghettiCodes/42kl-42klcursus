2a34ebc38d01c1e7c1f8bab5a73a8cd470bc35ae
I did not remember to change this :P <no way someone has this email wadjojwaosdjoadjwa@gmail.com>
2023-06-04 21:04:08 +0800

    cpp bsq
    whoops forgot to check if it one edge

--- 5fifth/cpp/module02/ex03/mapless/bsp.cpp
+++ 5fifth/cpp/module02/ex03/mapless/bsp.cpp
@@ -22,12 +22,29 @@ bool invalid( Point const &a, Point const &b, Point const &c, Point const &point
 	return (0);
 }
 
+bool in_sameline( Point const one, Point const two, Point const point)
+{
+	float	gradient;
+	float	intercept;
+
+	if ((one.getX() - two.getX()) == 0)
+		return (point.getX() == one.getX());
+	gradient = ((one.getY() - two.getY()) / (one.getX() - two.getX())).toFloat();
+	intercept = two.getY().toFloat() - (gradient * two.getX().toFloat());
+	return ((point.getY().toFloat() - (gradient * point.getX().toFloat())) == intercept);
+}
+
+bool in_edge( Point const a, Point const b, Point const c, Point const point)
+{
+	return (in_sameline(a, b, point) || in_sameline(b, c, point) || in_sameline(c, a, point));
+}
+
 // a, b, c vertices of triangle, point -> to search
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {	
 	bool val;
 	std::cout << "In bsp" << std::endl;
-	if (invalid(a, b, c, point))
+	if (invalid(a, b, c, point) || in_edge(a, b, c, point))
 		return 0;
 	find_point(a, b, c, point, val);
 	if (!val)
--- 5fifth/cpp/module02/ex03/mapless/main.cpp
+++ 5fifth/cpp/module02/ex03/mapless/main.cpp
@@ -10,10 +10,15 @@ int main( void )
 	Point a(5, 10);
 	Point b(1, 2);
 	Point c(3, 1);
+	Point tofind(3, 6);
+
+	std::cout << "To find = " << tofind << std::endl;
+
 	// Point tofind(2.6f, 4.2f);
-	Point tofind(4, 6);
-	// Point tofind(3.6f,5.4f);
-	// Point tofind(2.6f, 4.3f); 
+	// Point tofind(4, 6);
+	// // Point tofind(3.6f,5.4f);
+	// // Point tofind(2.6f, 4.3f); 
+
 
 	// Point a(100, 100);
 	// Point b(1, 1);
