8bf97c7b063238c4ed05e9543f4c5cc7110aaf8d
NotChuaSai <chuashixiang@gmail.com>
2023-10-03 12:05:57 +0800

    submitting 02 now

--- python/python02/completed/ex00/load_csv.py
+++ python/python02/completed/ex00/load_csv.py
@@ -9,7 +9,7 @@ def load(path: str) -> pd.DataFrame:
     @return | the dataframe extracted
     """
     name_segments = path.split('.')
-    if len(name_segments) < 2 or name_segments[1] != 'csv':
+    if len(name_segments) < 2 or name_segments[-1] != 'csv':
         print("WARNING -- File extension IS NOT CSV, you \
 sure you meant to load this file?")
 
@@ -21,7 +21,7 @@ sure you meant to load this file?")
         print(f"{e.strerror}: {e.filename}")
     except Exception as e:
         print("Something bad happened during parsing")
-        print("Error info --")
+        print("-- Error info --")
         print(e)
 
     return ret
